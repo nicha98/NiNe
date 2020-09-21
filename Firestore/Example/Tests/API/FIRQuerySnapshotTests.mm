0759f420427c2d06ffb1710130343d55efe96f40
NiNeW <69135009+nicha98@users.noreply.github.com>
2020-09-22 00:51:27 +0700

    Initialize varconst/test-fst-view-snapshot4

---
+++ Firestore/Example/Tests/API/FIRQuerySnapshotTests.mm
@@ -0,0 +1,147 @@
+/*
+ * Copyright 2017 Google
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#import <FirebaseFirestore/FIRQuerySnapshot.h>
+
+#import <XCTest/XCTest.h>
+
+#include <utility>
+#include <vector>
+
+#import "Firestore/Example/Tests/API/FSTAPIHelpers.h"
+#import "Firestore/Example/Tests/Util/FSTHelpers.h"
+#import "Firestore/Source/API/FIRDocumentChange+Internal.h"
+#import "Firestore/Source/API/FIRDocumentSnapshot+Internal.h"
+#import "Firestore/Source/API/FIRQuerySnapshot+Internal.h"
+#import "Firestore/Source/API/FIRSnapshotMetadata+Internal.h"
+#import "Firestore/Source/Model/FSTDocument.h"
+#import "Firestore/Source/Model/FSTDocumentSet.h"
+
+#include "Firestore/core/src/firebase/firestore/core/view_snapshot.h"
+#include "Firestore/core/src/firebase/firestore/util/string_apple.h"
+
+namespace util = firebase::firestore::util;
+using firebase::firestore::core::DocumentViewChange;
+using firebase::firestore::core::ViewSnapshot;
+using firebase::firestore::model::DocumentKeySet;
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FIRDocumentChange ()
+
+// Expose initializer for testing.
+- (instancetype)initWithType:(FIRDocumentChangeType)type
+                    document:(FIRQueryDocumentSnapshot *)document
+                    oldIndex:(NSUInteger)oldIndex
+                    newIndex:(NSUInteger)newIndex;
+
+@end
+
+@interface FIRQuerySnapshotTests : XCTestCase
+@end
+
+@implementation FIRQuerySnapshotTests
+
+- (void)testEquals {
+  FIRQuerySnapshot *foo = FSTTestQuerySnapshot("foo", @{}, @{@"a" : @{@"a" : @1}}, YES, NO);
+  FIRQuerySnapshot *fooDup = FSTTestQuerySnapshot("foo", @{}, @{@"a" : @{@"a" : @1}}, YES, NO);
+  FIRQuerySnapshot *differentPath =
+      FSTTestQuerySnapshot("bar", @{}, @{@"a" : @{@"a" : @1}}, YES, NO);
+  FIRQuerySnapshot *differentDoc =
+      FSTTestQuerySnapshot("foo", @{@"a" : @{@"b" : @1}}, @{}, YES, NO);
+  FIRQuerySnapshot *noPendingWrites =
+      FSTTestQuerySnapshot("foo", @{}, @{@"a" : @{@"a" : @1}}, NO, NO);
+  FIRQuerySnapshot *fromCache = FSTTestQuerySnapshot("foo", @{}, @{@"a" : @{@"a" : @1}}, YES, YES);
+  XCTAssertEqualObjects(foo, fooDup);
+  XCTAssertNotEqualObjects(foo, differentPath);
+  XCTAssertNotEqualObjects(foo, differentDoc);
+  XCTAssertNotEqualObjects(foo, noPendingWrites);
+  XCTAssertNotEqualObjects(foo, fromCache);
+
+  XCTAssertEqual([foo hash], [fooDup hash]);
+  XCTAssertNotEqual([foo hash], [differentPath hash]);
+  XCTAssertNotEqual([foo hash], [differentDoc hash]);
+  XCTAssertNotEqual([foo hash], [noPendingWrites hash]);
+  XCTAssertNotEqual([foo hash], [fromCache hash]);
+}
+
+- (void)testIncludeMetadataChanges {
+  FSTDocument *doc1Old = FSTTestDoc("foo/bar", 1, @{@"a" : @"b"}, FSTDocumentStateLocalMutations);
+  FSTDocument *doc1New = FSTTestDoc("foo/bar", 1, @{@"a" : @"b"}, FSTDocumentStateSynced);
+
+  FSTDocument *doc2Old = FSTTestDoc("foo/baz", 1, @{@"a" : @"b"}, FSTDocumentStateSynced);
+  FSTDocument *doc2New = FSTTestDoc("foo/baz", 1, @{@"a" : @"c"}, FSTDocumentStateSynced);
+
+  FSTDocumentSet *oldDocuments = FSTTestDocSet(FSTDocumentComparatorByKey, @[ doc1Old, doc2Old ]);
+  FSTDocumentSet *newDocuments = FSTTestDocSet(FSTDocumentComparatorByKey, @[ doc2New, doc2New ]);
+  std::vector<DocumentViewChange> documentChanges{
+      DocumentViewChange{doc1New, DocumentViewChange::Type::kMetadata},
+      DocumentViewChange{doc2New, DocumentViewChange::Type::kModified},
+  };
+
+  FIRFirestore *firestore = FSTTestFirestore();
+  FSTQuery *query = FSTTestQuery("foo");
+  ViewSnapshot viewSnapshot{query,
+                            newDocuments,
+                            oldDocuments,
+                            std::move(documentChanges),
+                            /*mutated_keys=*/DocumentKeySet{},
+                            /*from_cache=*/false,
+                            /*sync_state_changed=*/true,
+                            /*excludes_metadata_changes=*/false};
+  FIRSnapshotMetadata *metadata = [FIRSnapshotMetadata snapshotMetadataWithPendingWrites:NO
+                                                                               fromCache:NO];
+  FIRQuerySnapshot *snapshot = [FIRQuerySnapshot snapshotWithFirestore:firestore
+                                                         originalQuery:query
+                                                              snapshot:std::move(viewSnapshot)
+                                                              metadata:metadata];
+
+  FIRQueryDocumentSnapshot *doc1Snap = [FIRQueryDocumentSnapshot snapshotWithFirestore:firestore
+                                                                           documentKey:doc1New.key
+                                                                              document:doc1New
+                                                                             fromCache:NO
+                                                                      hasPendingWrites:NO];
+  FIRQueryDocumentSnapshot *doc2Snap = [FIRQueryDocumentSnapshot snapshotWithFirestore:firestore
+                                                                           documentKey:doc2New.key
+                                                                              document:doc2New
+                                                                             fromCache:NO
+                                                                      hasPendingWrites:NO];
+
+  NSArray<FIRDocumentChange *> *changesWithoutMetadata = @[
+    [[FIRDocumentChange alloc] initWithType:FIRDocumentChangeTypeModified
+                                   document:doc2Snap
+                                   oldIndex:1
+                                   newIndex:1],
+  ];
+  XCTAssertEqualObjects(snapshot.documentChanges, changesWithoutMetadata);
+
+  NSArray<FIRDocumentChange *> *changesWithMetadata = @[
+    [[FIRDocumentChange alloc] initWithType:FIRDocumentChangeTypeModified
+                                   document:doc1Snap
+                                   oldIndex:0
+                                   newIndex:0],
+    [[FIRDocumentChange alloc] initWithType:FIRDocumentChangeTypeModified
+                                   document:doc2Snap
+                                   oldIndex:1
+                                   newIndex:1],
+  ];
+  XCTAssertEqualObjects([snapshot documentChangesWithIncludeMetadataChanges:YES],
+                        changesWithMetadata);
+}
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Firestore/core/src/firebase/firestore/core/view_snapshot.h
@@ -0,0 +1,227 @@
+/*
+ * Copyright 2019 Google
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef FIRESTORE_CORE_SRC_FIREBASE_FIRESTORE_CORE_VIEW_SNAPSHOT_H_
+#define FIRESTORE_CORE_SRC_FIREBASE_FIRESTORE_CORE_VIEW_SNAPSHOT_H_
+
+#if !defined(__OBJC__)
+#error "This header only supports Objective-C++"
+#endif  // !defined(__OBJC__)
+
+#include <functional>
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "Firestore/core/src/firebase/firestore/immutable/sorted_map.h"
+#include "Firestore/core/src/firebase/firestore/model/document_key.h"
+#include "Firestore/core/src/firebase/firestore/model/document_key_set.h"
+#include "Firestore/core/src/firebase/firestore/util/statusor.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@class FSTDocument;
+@class FSTQuery;
+@class FSTDocumentSet;
+
+namespace firebase {
+namespace firestore {
+namespace core {
+
+/** A change to a single document's state within a view. */
+class DocumentViewChange {
+ public:
+  /**
+   * The types of changes that can happen to a document with respect to a view.
+   * NOTE: We sort document changes by their type, so the ordering of this enum
+   * is significant.
+   */
+  enum class Type { kRemoved = 0, kAdded, kModified, kMetadata };
+
+  DocumentViewChange() = default;
+
+  DocumentViewChange(FSTDocument* document, Type type)
+      : document_{document}, type_{type} {
+  }
+
+  FSTDocument* document() const {
+    return document_;
+  }
+  DocumentViewChange::Type type() const {
+    return type_;
+  }
+
+  std::string ToString() const;
+  size_t Hash() const;
+
+ private:
+  FSTDocument* document_ = nullptr;
+  Type type_{};
+};
+
+bool operator==(const DocumentViewChange& lhs, const DocumentViewChange& rhs);
+
+/**
+ * The possible states a document can be in w.r.t syncing from local storage to
+ * the backend.
+ */
+enum class SyncState { None = 0, Local, Synced };
+
+/**
+ * A set of changes to docs in a query, merging duplicate events for the same
+ * doc.
+ */
+class DocumentViewChangeSet {
+ public:
+  /** Takes a new change and applies it to the set. */
+  void AddChange(DocumentViewChange&& change);
+
+  /** Returns the set of all changes tracked in this set. */
+  std::vector<DocumentViewChange> GetChanges() const;
+
+  std::string ToString() const;
+
+ private:
+  /** The set of all changes tracked so far, with redundant changes merged. */
+  immutable::SortedMap<model::DocumentKey, DocumentViewChange> change_map_;
+};
+
+class ViewSnapshot;
+
+using ViewSnapshotHandler =
+    std::function<void(const util::StatusOr<ViewSnapshot>&)>;
+
+/**
+ * A view snapshot is an immutable capture of the results of a query and the
+ * changes to them.
+ */
+class ViewSnapshot {
+ public:
+  ViewSnapshot() = default;
+
+  ViewSnapshot(FSTQuery* query,
+               FSTDocumentSet* documents,
+               FSTDocumentSet* old_documents,
+               std::vector<DocumentViewChange> document_changes,
+               model::DocumentKeySet mutated_keys,
+               bool from_cache,
+               bool sync_state_changed,
+               bool excludes_metadata_changes);
+
+  /**
+   * Returns a view snapshot as if all documents in the snapshot were
+   * added.
+   */
+  static ViewSnapshot FromInitialDocuments(FSTQuery* query,
+                                           FSTDocumentSet* documents,
+                                           model::DocumentKeySet mutated_keys,
+                                           bool from_cache,
+                                           bool excludes_metadata_changes);
+
+  /** The query this view is tracking the results for. */
+  FSTQuery* query() const {
+    return impl_->query;
+  }
+
+  /** The documents currently known to be results of the query. */
+  FSTDocumentSet* documents() const {
+    return impl_->documents;
+  }
+
+  /** The documents of the last snapshot. */
+  FSTDocumentSet* old_documents() const {
+    return impl_->old_documents;
+  }
+
+  /** The set of changes that have been applied to the documents. */
+  const std::vector<DocumentViewChange>& document_changes() const {
+    return impl_->document_changes;
+  }
+
+  /** Whether any document in the snapshot was served from the local cache. */
+  bool from_cache() const {
+    return impl_->from_cache;
+  }
+
+  /** Whether any document in the snapshot has pending local writes. */
+  bool has_pending_writes() const {
+    return !(impl_->mutated_keys.empty());
+  }
+
+  /** Whether the sync state changed as part of this snapshot. */
+  bool sync_state_changed() const {
+    return impl_->sync_state_changed;
+  }
+
+  /** Whether this snapshot has been filtered to not include metadata changes */
+  bool excludes_metadata_changes() const {
+    return impl_->excludes_metadata_changes;
+  }
+
+  /** The document in this snapshot that have unconfirmed writes. */
+  model::DocumentKeySet mutated_keys() const {
+    return impl_->mutated_keys;
+  }
+
+  std::string ToString() const;
+  size_t Hash() const;
+
+  friend bool operator==(const ViewSnapshot& lhs, const ViewSnapshot& rhs);
+
+ private:
+  struct Impl {
+    Impl(FSTQuery* query,
+         FSTDocumentSet* documents,
+         FSTDocumentSet* old_documents,
+         std::vector<DocumentViewChange> document_changes,
+         bool from_cache,
+         bool sync_state_changed,
+         bool excludes_metadata_changes,
+         model::DocumentKeySet mutated_keys)
+        : query{query},
+          documents{documents},
+          old_documents{old_documents},
+          document_changes{std::move(document_changes)},
+          from_cache{from_cache},
+          sync_state_changed{sync_state_changed},
+          excludes_metadata_changes{excludes_metadata_changes},
+          mutated_keys{std::move(mutated_keys)} {
+    }
+
+    FSTQuery* query = nil;
+
+    FSTDocumentSet* documents = nil;
+    FSTDocumentSet* old_documents = nil;
+    std::vector<DocumentViewChange> document_changes;
+
+    bool from_cache = false;
+    bool sync_state_changed = false;
+    bool excludes_metadata_changes = false;
+
+    model::DocumentKeySet mutated_keys;
+  };
+
+  std::shared_ptr<const Impl> impl_;
+};
+
+}  // namespace core
+}  // namespace firestore
+}  // namespace firebase
+
+NS_ASSUME_NONNULL_END
+
+#endif  // FIRESTORE_CORE_SRC_FIREBASE_FIRESTORE_CORE_VIEW_SNAPSHOT_H_
