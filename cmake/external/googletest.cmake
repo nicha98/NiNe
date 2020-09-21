93464abec6af34c2a645fe7370e0b18e96986d9e
NiNeW <69135009+nicha98@users.noreply.github.com>
2020-09-22 00:25:32 +0700

    Initialize staging-4.13.1

---
+++ cmake/external/googletest.cmake
@@ -0,0 +1,33 @@
+# Copyright 2017 Google
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+include(ExternalProject)
+
+ExternalProject_Add(
+  googletest
+
+  DOWNLOAD_DIR ${PROJECT_BINARY_DIR}/downloads
+  DOWNLOAD_NAME googletest-1.8.0.tar.gz
+  URL https://github.com/google/googletest/archive/release-1.8.0.tar.gz
+  URL_HASH SHA256=58a6f4277ca2bc8565222b3bbd58a177609e9c488e8a72649359ba51450db7d8
+
+  PREFIX ${PROJECT_BINARY_DIR}/external/googletest
+
+  # Just download the sources without building.
+  UPDATE_COMMAND ""
+  CONFIGURE_COMMAND ""
+  BUILD_COMMAND ""
+  INSTALL_COMMAND ""
+  TEST_COMMAND ""
+)
