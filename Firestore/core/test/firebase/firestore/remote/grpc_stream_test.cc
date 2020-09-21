603621516c91c913dd950aecdb7fdd4319d8d755
NiNeW <69135009+nicha98@users.noreply.github.com>
2020-09-22 00:38:45 +0700

    Initialize release-5.10.0

---
+++ Firestore/core/test/firebase/firestore/remote/grpc_stream_test.cc
@@ -0,0 +1,248 @@
+/*
+ * Copyright 2018 Google
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "Firestore/core/src/firebase/firestore/remote/grpc_stream.h"
+
+#include <initializer_list>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "Firestore/core/src/firebase/firestore/util/async_queue.h"
+#include "Firestore/core/test/firebase/firestore/util/grpc_stream_tester.h"
+#include "absl/memory/memory.h"
+#include "grpcpp/support/byte_buffer.h"
+#include "gtest/gtest.h"
+
+namespace firebase {
+namespace firestore {
+namespace remote {
+
+using util::AsyncQueue;
+using util::CompletionEndState;
+using util::GrpcStreamTester;
+using util::CompletionResult::Error;
+using util::CompletionResult::Ok;
+
+namespace {
+
+class Observer : public GrpcStreamObserver {
+ public:
+  void OnStreamStart() override {
+    observed_states.push_back("OnStreamStart");
+  }
+  void OnStreamRead(const grpc::ByteBuffer& message) override {
+    observed_states.push_back("OnStreamRead");
+  }
+  void OnStreamFinish(const util::Status& status) override {
+    observed_states.push_back("OnStreamFinish");
+  }
+
+  std::vector<std::string> observed_states;
+};
+
+}  // namespace
+
+class GrpcStreamTest : public testing::Test {
+ public:
+  GrpcStreamTest()
+      : observer_{absl::make_unique<Observer>()},
+        stream_{tester_.CreateStream(observer_.get())} {
+  }
+
+  ~GrpcStreamTest() {
+    if (!stream_->IsFinished()) {
+      KeepPollingGrpcQueue();
+      worker_queue().EnqueueBlocking([&] { stream_->Finish(); });
+    }
+    tester_.Shutdown();
+  }
+
+  GrpcStream& stream() {
+    return *stream_;
+  }
+  AsyncQueue& worker_queue() {
+    return tester_.worker_queue();
+  }
+
+  void ForceFinish(std::initializer_list<CompletionEndState> results) {
+    tester_.ForceFinish(results);
+  }
+  void KeepPollingGrpcQueue() {
+    tester_.KeepPollingGrpcQueue();
+  }
+  void ShutdownGrpcQueue() {
+    tester_.ShutdownGrpcQueue();
+  }
+
+  const std::vector<std::string>& observed_states() const {
+    return observer_->observed_states;
+  }
+
+  // This is to make `EXPECT_EQ` a little shorter and work around macro
+  // limitations related to initializer lists.
+  std::vector<std::string> States(std::initializer_list<std::string> states) {
+    return {states};
+  }
+
+  bool ObserverHas(const std::string& state) const {
+    return std::find(observed_states().begin(), observed_states().end(),
+                     state) != observed_states().end();
+  }
+
+  void StartStream() {
+    worker_queue().EnqueueBlocking([&] { stream().Start(); });
+  }
+
+ private:
+  GrpcStreamTester tester_;
+  std::unique_ptr<Observer> observer_;
+  std::unique_ptr<GrpcStream> stream_;
+};
+
+TEST_F(GrpcStreamTest, CanFinishBeforeStarting) {
+  worker_queue().EnqueueBlocking([&] { EXPECT_NO_THROW(stream().Finish()); });
+}
+
+TEST_F(GrpcStreamTest, CanFinishAfterStarting) {
+  StartStream();
+  KeepPollingGrpcQueue();
+
+  worker_queue().EnqueueBlocking([&] { EXPECT_NO_THROW(stream().Finish()); });
+}
+
+TEST_F(GrpcStreamTest, CanFinishTwice) {
+  worker_queue().EnqueueBlocking([&] {
+    EXPECT_NO_THROW(stream().Finish());
+    EXPECT_NO_THROW(stream().Finish());
+  });
+}
+
+TEST_F(GrpcStreamTest, CanWriteAndFinishAfterStarting) {
+  StartStream();
+  KeepPollingGrpcQueue();
+
+  worker_queue().EnqueueBlocking(
+      [&] { EXPECT_NO_THROW(stream().WriteAndFinish({})); });
+}
+
+TEST_F(GrpcStreamTest, ObserverReceivesOnStart) {
+  StartStream();
+  EXPECT_EQ(observed_states(), States({"OnStreamStart"}));
+}
+
+TEST_F(GrpcStreamTest, CanWriteAfterStreamIsOpen) {
+  StartStream();
+  worker_queue().EnqueueBlocking([&] { EXPECT_NO_THROW(stream().Write({})); });
+}
+
+TEST_F(GrpcStreamTest, ObserverReceivesOnRead) {
+  StartStream();
+  ForceFinish({/*Read*/ Ok});
+  EXPECT_EQ(observed_states(), States({"OnStreamStart", "OnStreamRead"}));
+}
+
+TEST_F(GrpcStreamTest, ReadIsAutomaticallyReadded) {
+  StartStream();
+  ForceFinish({/*Read*/ Ok});
+  EXPECT_EQ(observed_states(), States({"OnStreamStart", "OnStreamRead"}));
+
+  ForceFinish({/*Read*/ Ok});
+  EXPECT_EQ(observed_states(),
+            States({"OnStreamStart", "OnStreamRead", "OnStreamRead"}));
+}
+
+TEST_F(GrpcStreamTest, CanAddSeveralWrites) {
+  StartStream();
+
+  worker_queue().EnqueueBlocking([&] {
+    stream().Write({});
+    stream().Write({});
+    stream().Write({});
+  });
+  ForceFinish({/*Read*/ Ok, /*Write*/ Ok, /*Read*/ Ok, /*Write*/ Ok,
+               /*Read*/ Ok, /*Write*/ Ok});
+
+  EXPECT_EQ(observed_states(), States({"OnStreamStart", "OnStreamRead",
+                                       "OnStreamRead", "OnStreamRead"}));
+}
+
+TEST_F(GrpcStreamTest, ObserverReceivesOnError) {
+  StartStream();
+
+  // Fail the read, but allow the rest to succeed.
+  ForceFinish({/*Read*/ Error});  // Will put a "Finish" operation on the queue
+  KeepPollingGrpcQueue();
+  // Once gRPC queue shutdown succeeds, "Finish" operation is guaranteed to be
+  // extracted from gRPC completion queue (but the completion may not have run
+  // yet).
+  ShutdownGrpcQueue();
+  // Finally, ensure `GrpcCompletion` for "Finish" operation has a chance to run
+  // on the worker queue.
+  worker_queue().EnqueueBlocking([] {});
+
+  EXPECT_EQ(observed_states(), States({"OnStreamStart", "OnStreamFinish"}));
+}
+
+TEST_F(GrpcStreamTest, ObserverDoesNotReceiveOnFinishIfCalledByClient) {
+  StartStream();
+  KeepPollingGrpcQueue();
+
+  worker_queue().EnqueueBlocking([&] { stream().Finish(); });
+  EXPECT_FALSE(ObserverHas("OnStreamFinish"));
+}
+
+TEST_F(GrpcStreamTest, WriteAndFinish) {
+  StartStream();
+  KeepPollingGrpcQueue();
+
+  worker_queue().EnqueueBlocking([&] {
+    bool did_last_write = stream().WriteAndFinish({});
+    EXPECT_TRUE(did_last_write);
+
+    EXPECT_TRUE(ObserverHas("OnStreamStart"));
+    EXPECT_FALSE(ObserverHas("OnStreamFinish"));
+  });
+}
+
+TEST_F(GrpcStreamTest, ErrorOnWrite) {
+  StartStream();
+  worker_queue().EnqueueBlocking([&] { stream().Write({}); });
+
+  ForceFinish({/*Write*/ Error, /*Read*/ Error});
+  // Give `GrpcStream` a chance to enqueue a finish operation
+  ForceFinish({/*Finish*/ Ok});
+
+  EXPECT_EQ(observed_states().back(), "OnStreamFinish");
+}
+
+TEST_F(GrpcStreamTest, ErrorWithPendingWrites) {
+  StartStream();
+  worker_queue().EnqueueBlocking([&] {
+    stream().Write({});
+    stream().Write({});
+  });
+
+  ForceFinish({/*Write*/ Ok, /*Write*/ Error});
+  // Give `GrpcStream` a chance to enqueue a finish operation
+  ForceFinish({/*Read*/ Error, /*Finish*/ Ok});
+
+  EXPECT_EQ(observed_states().back(), "OnStreamFinish");
+}
+
+}  // namespace remote
+}  // namespace firestore
+}  // namespace firebase
---
+++ Firestore/core/test/firebase/firestore/util/grpc_stream_tester.cc
@@ -0,0 +1,119 @@
+/*
+ * Copyright 2018 Google
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "Firestore/core/test/firebase/firestore/util/grpc_stream_tester.h"
+
+#include <utility>
+
+#include "absl/memory/memory.h"
+
+namespace firebase {
+namespace firestore {
+namespace util {
+
+using internal::ExecutorStd;
+using remote::GrpcCompletion;
+using remote::GrpcStream;
+using remote::GrpcStreamingReader;
+using remote::GrpcStreamObserver;
+
+GrpcStreamTester::GrpcStreamTester()
+    : worker_queue_{absl::make_unique<ExecutorStd>()},
+      dedicated_executor_{absl::make_unique<ExecutorStd>()},
+      grpc_stub_{grpc::CreateChannel("", grpc::InsecureChannelCredentials())} {
+}
+
+GrpcStreamTester::~GrpcStreamTester() {
+  // Make sure the stream and gRPC completion queue are properly shut down.
+  Shutdown();
+}
+
+void GrpcStreamTester::Shutdown() {
+  worker_queue_.EnqueueBlocking([&] { ShutdownGrpcQueue(); });
+}
+
+std::unique_ptr<GrpcStream> GrpcStreamTester::CreateStream(
+    GrpcStreamObserver* observer) {
+  auto grpc_context_owning = absl::make_unique<grpc::ClientContext>();
+  grpc_context_ = grpc_context_owning.get();
+  auto grpc_call =
+      grpc_stub_.PrepareCall(grpc_context_owning.get(), "", &grpc_queue_);
+
+  return absl::make_unique<GrpcStream>(std::move(grpc_context_owning),
+                                       std::move(grpc_call), observer,
+                                       &worker_queue_);
+}
+
+std::unique_ptr<GrpcStreamingReader> GrpcStreamTester::CreateStreamingReader() {
+  auto grpc_context_owning = absl::make_unique<grpc::ClientContext>();
+  grpc_context_ = grpc_context_owning.get();
+  auto grpc_call =
+      grpc_stub_.PrepareCall(grpc_context_owning.get(), "", &grpc_queue_);
+
+  return absl::make_unique<GrpcStreamingReader>(
+      std::move(grpc_context_owning), std::move(grpc_call), &worker_queue_,
+      grpc::ByteBuffer{});
+}
+
+void GrpcStreamTester::ShutdownGrpcQueue() {
+  if (is_shut_down_) {
+    return;
+  }
+  is_shut_down_ = true;
+
+  grpc_queue_.Shutdown();
+  // Wait for gRPC completion queue to drain
+  dedicated_executor_->ExecuteBlocking([] {});
+}
+
+// This is a very hacky way to simulate gRPC finishing operations without
+// actually connecting to the server: cancel the stream, which will make all
+// operations fail fast and be returned from the completion queue, then
+// complete the associated completion.
+void GrpcStreamTester::ForceFinish(
+    std::initializer_list<CompletionEndState> end_states) {
+  dedicated_executor_->ExecuteBlocking([&] {
+    // gRPC allows calling `TryCancel` more than once.
+    grpc_context_->TryCancel();
+
+    for (CompletionEndState end_state : end_states) {
+      bool ignored_ok = false;
+      void* tag = nullptr;
+      grpc_queue_.Next(&tag, &ignored_ok);
+      auto completion = static_cast<remote::GrpcCompletion*>(tag);
+      if (end_state.maybe_status) {
+        *completion->status() = end_state.maybe_status.value();
+      }
+      completion->Complete(end_state.result == CompletionResult::Ok);
+    }
+  });
+
+  worker_queue_.EnqueueBlocking([] {});
+}
+
+void GrpcStreamTester::KeepPollingGrpcQueue() {
+  dedicated_executor_->Execute([&] {
+    void* tag = nullptr;
+    bool ignored_ok = false;
+    while (grpc_queue_.Next(&tag, &ignored_ok)) {
+      static_cast<GrpcCompletion*>(tag)->Complete(true);
+    }
+  });
+}
+
+}  // namespace util
+}  // namespace firestore
+}  // namespace firebase
