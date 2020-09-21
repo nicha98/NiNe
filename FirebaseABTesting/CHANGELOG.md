9808c6b912b13fcee5b65f20218d652976c5c500
NiNeW <69135009+nicha98@users.noreply.github.com>
2020-09-22 02:43:38 +0700

    Initialize kokoro

---
+++ FirebaseABTesting/CHANGELOG.md
@@ -0,0 +1,11 @@
+# v3.2.0
+- [added] Added completion handler for FIRExperimentController's updateExperimentsWithServiceOrigin method.
+- [deprecated] Deprecated `FIRExperimentController.updateExperiments(serviceOrigin:events:policy:lastStartTime:payloads:)`.
+- [added] Added method to expire experiments that are no longer running.
+- [added] Added method to directly activate an experiment.
+
+# v3.1.1
+- [fixed] Fixed an Analyzer issue (#3622).
+
+# v3.1.0
+- [added] Initial Open Source (#3507).
---
+++ kokoro/macos_external/continuous.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Copyright 2019 Google
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#      http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Fail on any error.
+set -e
+# Display commands being run.
+set -x
+
+# Placeholder script to test GitHub webhooks. Eventually this script
+# should be replaced by one job per test target.
+
+# Here's an example of one build target in travis manually migrated
+# to kokoro with no shared components:
+
+cd ${KOKORO_ARTIFACTS_DIR}/github/firebase-ios-sdk
+
+export PROJECT="Firestore"
+export PLATFORM="iOS"
+export METHOD="xcodebuild"
+
+# before_install:
+./kokoro/shared/before_install.sh
+
+# Force xcpretty to use UTF8
+export LC_CTYPE=en_US.UTF-8
+
+pushd ZipBuilder
+
+bundle install
+pod repo update
+swift run ReleasePackager -zipPods "../kokoro/shared/jazzy_build.json" \
+  -templateDir $(pwd)/Template \
+  -updatePodRepo false
+
+popd
